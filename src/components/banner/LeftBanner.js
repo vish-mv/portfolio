9a7831a4346a4e7dfaa4cd5d5b72b4e34af55ecb
Visal Munasinghe <127712545+vish-mv@users.noreply.github.com>
2024-03-25 18:46:58 +0530

    Change Banners

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -4,7 +4,7 @@ import Media from './Media';
 
 const LeftBanner = () => {
     const [text] = useTypewriter({
-      words: ["Professional Coder.", "Full Stack Developer.", "UI Designer."],
+      words: ["Coder.", "Front End Developer.", "UI Designer."],
       loop: true,
       typeSpeed: 20,
       deleteSpeed: 10,
@@ -13,9 +13,9 @@ const LeftBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex flex-col gap-20">
       <div className="flex flex-col gap-5">
-        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h4 className=" text-lg font-normal">WELCOME TO MY PORTFOLIO</h4>
         <h1 className="text-6xl font-bold text-white">
-          Hi, I'm <span className="text-designColor capitalize">John doe</span>
+          Hi, I'm <span className="text-designColor capitalize">Visal Vitharana</span>
         </h1>
         <h2 className="text-4xl font-bold text-white">
           a <span>{text}</span>
