1963300e75291b34df2b180b1d49b3f4184bf838
Visal Munasinghe <127712545+vish-mv@users.noreply.github.com>
2024-03-25 21:35:15 +0530

    Update

--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF, FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { contactImg } from "../../assets/index";
 
 const ContactLeft = () => {
@@ -11,19 +11,18 @@ const ContactLeft = () => {
         alt="contactImg"
       />
       <div className="flex flex-col gap-4">
-        <h3 className="text-3xl font-bold text-white">John Doe</h3>
+        <h3 className="text-3xl font-bold text-white">Visal Vitharana</h3>
         <p className="text-lg font-normal text-gray-400">
-          MERN Stack Developer
+          Undergraduate At University of Westminster
         </p>
         <p className="text-base text-gray-400 tracking-wide">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
-          ipsam autem cumque, accusantium dicta odio.
+        As a Software Engineering student proficient in Java and Python, I specialize in captivating web designs. My passion lies in front-end development, particularly with React and Bootstrap. And I also Interests in Machine learning and AI.
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Phone: <span className="text-lightText">+968 97859628</span>
+          Phone: <span className="text-lightText">+94 76 712 3972</span>
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Email: <span className="text-lightText">noor.jsdivs@gmail.com</span>
+          Email: <span className="text-lightText">visalmunasinghe@gmail.com</span>
         </p>
       </div>
       <div className="flex flex-col gap-4">
@@ -33,7 +32,7 @@ const ContactLeft = () => {
             <FaFacebookF />
           </span>
           <span className="bannerIcon">
-            <FaTwitter />
+            <FaGithub />
           </span>
           <span className="bannerIcon">
             <FaLinkedinIn />
